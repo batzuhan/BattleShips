ee0520a35f337d2cfcee6ba98160632adcb47181
Batzu <batuhan.ozcan@ozu.edu.tr>
2019-01-26 21:00:53 +0200

    Made correction.

--- Model/Player/PlayerComputer.java
+++ Model/Player/PlayerComputer.java
@@ -13,13 +13,13 @@ public class PlayerComputer extends Player{
 
     @Override
     public void placeShips(Field otherField) {
-        AircraftCarrier a1 = new AircraftCarrier(5,5,"A",getOpponentField(),null,null);
-        AircraftCarrier a2 = new AircraftCarrier(5,5,"A",getOpponentField(),null,null);
-        Destroyer d1 = new Destroyer(3,2,"D",getOpponentField(),null,null);
-        Destroyer d2 = new Destroyer(3,2,"D",getOpponentField(),null,null);
-        Destroyer d3 = new Destroyer(3,2,"D",getOpponentField(),null,null);
-        Submarine s1 = new Submarine(1,3,"S",getOpponentField(),null,null);
-        Submarine s2 = new Submarine(1,3,"S",getOpponentField(),null,null);
+        AircraftCarrier a1 = new AircraftCarrier(5,5,"A",otherField,null,null);
+        AircraftCarrier a2 = new AircraftCarrier(5,5,"A",otherField,null,null);
+        Destroyer d1 = new Destroyer(3,2,"D",otherField,null,null);
+        Destroyer d2 = new Destroyer(3,2,"D",otherField,null,null);
+        Destroyer d3 = new Destroyer(3,2,"D",otherField,null,null);
+        Submarine s1 = new Submarine(1,3,"S",otherField,null,null);
+        Submarine s2 = new Submarine(1,3,"S",otherField,null,null);
         otherField.placeShipRandomly(a1,3);
         otherField.placeShipRandomly(a2,3);
         otherField.placeShipRandomly(d1,3);
